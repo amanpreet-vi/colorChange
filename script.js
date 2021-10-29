9c8179e68404e67f5fb9a07eb484491ed48ad31f
amanpreet-vi <amanpreet3338@gmail.com>
2021-10-29 13:02:20 +0530

    files are changed

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const td = document.querySelectorAll("td");
-
+const arr = [];
 td.forEach((value, index) => {
-  value.style.backgroundColor = random();
+  value.onclick = () => {
+    colors();
+    value.style.backgroundColor = "red";
+    arr.push(index);
+  };
 });
-
+function colors() {
+  arr.forEach((number) => {
+    td[number].style.backgroundColor = random();
+  });
+}
 function random() {
   const string = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
   return string;
 }
+
 function start() {
   document.title = "randomColors";
 }
